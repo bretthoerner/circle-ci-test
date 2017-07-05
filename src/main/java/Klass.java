fe4e5c011ee343eb9358d52fa707c2e1d975a7ab
Brett Hoerner <brett@bretthoerner.com>
2017-07-05 14:16:14 -0500

    Add arch.

--- src/main/java/Klass.java
+++ src/main/java/Klass.java
@@ -1,5 +1,6 @@
 public class Klass {
     public static void thing() {
         System.err.println(System.getProperty("os.name"));
+        System.err.println(System.getProperty("os.arch"));
     }
 }
